562975e41f83f9d908a68307274d2b3a7e2008dc
LmDev99 <LoganMottern4@hotmail.com>
2026-06-15 16:55:31 -0500

    Update misc.h

--- misc.h
+++ misc.h
@@ -1,10 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2026 Logan Mottern
- * All rights reserved.
+ * LinuxRenderer-GraphicalEnvironment - custom Linux/Vulkan rendering and UI engine
+ * Copyright (C) 2026 Logan Mottern
  *
- * This source code is provided for portfolio and review purposes only.
- * No permission is granted to copy, modify, distribute, sublicense, or use
- * this software except with the express written permission of the copyright owner.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
